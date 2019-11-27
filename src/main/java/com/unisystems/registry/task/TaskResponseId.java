71072de253d0a510a92a94063dd89f035677d1ea
George Portokallas <gportokallas@gmail.com>
2019-11-27 22:54:38 +0200

    TaskServiceShould Test Ready plus some files that i didnt include before

---
+++ src/main/java/com/unisystems/registry/task/TaskResponseId.java
@@ -0,0 +1,82 @@
+package com.unisystems.registry.task;
+
+import com.unisystems.registry.employee.Employee;
+import com.unisystems.registry.employee.EmployeeResponse;
+
+import java.util.List;
+
+public class TaskResponseId {
+    private long id;
+    private String title;
+    private String desc;
+    private String difficulty;
+    private String status;
+    private List<EmployeeResponse> assignedEmployees;
+    private List<String> updates;
+
+    public TaskResponseId(long id, String title, String desc, String difficulty, String status, List<EmployeeResponse> assignedEmployees, List<String> updates) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.difficulty = difficulty;
+        this.status = status;
+        this.assignedEmployees = assignedEmployees;
+        this.updates = updates;
+    }
+
+    public List<EmployeeResponse> getAssignedEmployees() {
+        return assignedEmployees;
+    }
+
+    public void setAssignedEmployees(List<EmployeeResponse> assignedEmployees) {
+        this.assignedEmployees = assignedEmployees;
+    }
+
+    public List<String> getUpdates() {
+        return updates;
+    }
+
+    public void setUpdates(List<String> updates) {
+        this.updates = updates;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/com/unisystems/registry/tasks/TaskMapperShould.java
@@ -0,0 +1,30 @@
+package com.unisystems.registry.tasks;
+
+import com.unisystems.registry.task.Status;
+import com.unisystems.registry.task.Task;
+import com.unisystems.registry.task.TaskMapper;
+import com.unisystems.registry.task.TaskResponse;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TaskMapperShould {
+    private TaskMapper mapper;
+    private Task taskInput;
+    private TaskResponse expectedOutput;
+
+    @Before
+    public void setUp(){
+        mapper = new TaskMapper();
+        taskInput = new Task("Android App For New Product", "Create an android app for the new product.",3,2,2, Status.NEW,null);
+        taskInput.setId(1);
+        expectedOutput = new TaskResponse(1,"Android App For New Product", "Create an android app for the new product.","MEDIUM", "New");
+    }
+
+    @Test
+    public void mapTaskFromTaskResponse(){
+        TaskResponse output = mapper.mapTask(taskInput);
+        Assert.assertThat(expectedOutput,Matchers.samePropertyValuesAs(output));
+    }
+}
