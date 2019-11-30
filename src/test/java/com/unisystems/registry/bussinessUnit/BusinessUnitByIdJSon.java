9daa8b82dfe65119fc7273c9017d83a39083de4b
tra3dimi <stratigopoulou_dimitra@yahoo.com>
2019-11-30 12:22:34 +0200

    change in businessUnitByIdJSon

--- src/test/java/com/unisystems/registry/bussinessUnit/BusinessUnitByIdJSon.java
+++ src/test/java/com/unisystems/registry/bussinessUnit/BusinessUnitByIdJSon.java
@@ -3,10 +3,7 @@ package com.unisystems.registry.bussinessUnit;
 public class BusinessUnitByIdJSon {
     public static String json = "{\n" +
             "    \"id\": 2,\n" +
-            "    \"name\": \"Telecommunications\",\n" +
-            "    \"company\": {\n" +
-            "        \"id\": 1,\n" +
-            "        \"name\": \"UniSystems\"\n" +
-            "    }\n" +
+            "    \"businessUnitName\": \"Telecommunications\",\n" +
+            "    \"companyName\": \"UniSystems\"\n" +
             "}";
 }
